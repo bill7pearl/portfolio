95aa4f9d8161d3b7aaf2d2af399ddbf9a17c2226
Billal <billal7chami@gmail.com>
2022-11-15 12:09:10 +0100

    Add eslint and fix errors

--- app.js
+++ app.js
@@ -1,25 +1,25 @@
-let menu = document.querySelector(".mobile-menu");
-let menuItems = document.querySelectorAll(".menu-item");
-let hamburger= document.querySelector(".hamburger");
-let closeIcon= document.querySelector(".close-icon");
-let menuIcon = document.querySelector(".menu-icon");
+const menu = document.querySelector('.mobile-menu');
+const menuItems = document.querySelectorAll('.menu-item');
+const hamburger = document.querySelector('.hamburger');
+const closeIcon = document.querySelector('.close-icon');
+const menuIcon = document.querySelector('.menu-icon');
 
 function toggleMenu() {
-    if (menu.classList.contains("show-menu")) {
-      menu.classList.remove("show-menu");
-      closeIcon.style.display = "none";
-      menuIcon.style.display = "block";
-    } else {
-      menu.classList.add("show-menu");
-      closeIcon.style.display = "block";
-      menuIcon.style.display = "none";
-    }
+  if (menu.classList.contains('show-menu')) {
+    menu.classList.remove('show-menu');
+    closeIcon.style.display = 'none';
+    menuIcon.style.display = 'block';
+  } else {
+    menu.classList.add('show-menu');
+    closeIcon.style.display = 'block';
+    menuIcon.style.display = 'none';
   }
-  
-  hamburger.addEventListener("click", toggleMenu);
-  
-  menuItems.forEach( 
-    function(menuItem) { 
-      menuItem.addEventListener("click", toggleMenu);
-    }
-  )
+}
+
+hamburger.addEventListener('click', toggleMenu);
+
+menuItems.forEach(
+  (menuItem) => {
+    menuItem.addEventListener('click', toggleMenu);
+  },
+);
