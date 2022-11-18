b767fd167a7bdcfd2bb195936e7a0e9956fb8c3e
Billal <billal7chami@gmail.com>
2022-11-18 15:43:45 +0100

    Add modal pop ups functions and data

--- app.js
+++ app.js
@@ -1,8 +1,3 @@
-window.onload = (event) =>{
-
-  console.log('Page Loaded');
-
-};
 const menu = document.querySelector('.mobile-menu');
 const menuItems = document.querySelectorAll('.menu-item');
 const hamburger = document.querySelector('.hamburger');
@@ -72,3 +67,114 @@ const projectsData = [
     seeSource: '#',
   },
 ];
+
+// projects container //
+
+const portfolio = document.getElementById('portfolio');
+const projects = `
+   <div class="project1">
+   <div class="project1-desk-left">
+       <img class="hide-from-desk" src="${projectsData[0].image}" alt="portfolio one">
+   </div>
+   <div class="project1-desk-right">
+       <h2 class="tonic-title">${projectsData[0].title}</h2>
+    <div class="infos">
+       <p class="canopy">${projectsData[0].infos[0]}</p>
+       <img class="dot" src="images/dot.png" alt="separator">
+       <p class="tech-date">${projectsData[0].infos[1]}</p>
+       <img class="dot" src="images/dot.png" alt="separator">
+       <p class="tech-date">${projectsData[0].infos[2]}</p>  
+    </div>
+   <p class="projects-description">A daily selection of privately personalized reads; no accounts or sign-ups required.</p>
+    <ul class="project-languages">
+       <li class="project-list">${projectsData[0].languages[0]}</li>
+       <li class="project-list">${projectsData[0].languages[1]}</li>
+       <li class="project-list">${projectsData[0].languages[2]}</li>
+    </ul>
+    <button onclick="showModal()" class="projects-btn" id="btn-project-1">See Project</button>
+   </div> 
+ </div>
+ 
+ <div class="project2">
+   <div class="project2-desk-left">
+       <img class="hide-from-desk" src="${projectsData[1].image}" alt="portfolio two">
+   </div>
+   <div class="project2-desk-right">
+       <h2 class="projects-title">${projectsData[1].title}</h2>
+    <div class="infos">
+       <p class="canopy">${projectsData[1].infos[0]}</p>
+       <img class="dot" src="images/dot.png" alt="separator">
+       <p class="tech-date">${projectsData[1].infos[1]}</p>
+       <img class="dot" src="images/dot.png" alt="separator">
+       <p class="tech-date">${projectsData[1].infos[2]}</p>  
+    </div>
+   <p class="projects-description">A daily selection of privately personalized reads;
+        no accounts or sign-ups required.</p>
+    <ul class="project-languages">
+       <li class="project-list">${projectsData[1].languages[0]}</li>
+       <li class="project-list">${projectsData[1].languages[1]}</li>
+       <li class="project-list">${projectsData[1].languages[2]}</li>
+    </ul>
+    <button onclick="showModal()" class="projects-btn" id="btn-project-2">See Project</button>
+   </div>
+ </div>
+   
+ <div class="project3">
+   <div class="project3-desk-left">
+       <img class="hide-from-desk" src="${projectsData[2].image}" alt="portfolio three">
+   </div>
+   <div class="project3-desk-right">
+       <h2 class="projects-title">${projectsData[2].title}</h2>
+   <div class="infos">
+       <p class="canopy">${projectsData[2].infos[0]}</p>
+       <img class="dot" src="images/dot.png" alt="separator">
+       <p class="tech-date">${projectsData[2].infos[1]}</p>
+       <img class="dot" src="images/dot.png" alt="separator">
+       <p class="tech-date">${projectsData[2].infos[2]}</p>  
+    </div>
+   <p class="projects-description">A daily selection of privately personalized reads;
+        no accounts or sign-ups required.</p>
+    <ul class="project-languages">
+       <li class="project-list">${projectsData[2].languages[0]}</li>
+       <li class="project-list">${projectsData[2].languages[1]}</li>
+       <li class="project-list">${projectsData[2].languages[2]}</li>
+    </ul>
+    <button onclick="showModal()" class="projects-btn">See Project</button>
+   </div>
+ </div>
+ 
+ <div class="project4">
+   <div class="project4-desk-left">
+       <img class="hide-from-desk" src="${projectsData[3].image}" alt="portfolio four">
+   </div>
+   <div class="project4-desk-right">
+       <h2 class="projects-title">${projectsData[3].title}</h2>
+   <div class="infos">
+       <p class="canopy">${projectsData[3].infos[0]}</p>
+       <img class="dot" src="images/dot.png" alt="separator">
+       <p class="tech-date">${projectsData[3].infos[1]}</p>
+       <img class="dot" src="images/dot.png" alt="separator">
+       <p class="tech-date">${projectsData[3].infos[2]}</p>  
+    </div>
+   <p class="projects-description">A daily selection of privately personalized reads;
+        no accounts or sign-ups required.</p>
+    <ul class="project-languages">
+       <li class="project-list">${projectsData[3].languages[0]}</li>
+       <li class="project-list">${projectsData[3].languages[1]}</li>
+       <li class="project-list">${projectsData[3].languages[2]}</li>
+    </ul>
+    <button onclick="showModal()" class="projects-btn">See Project</button>
+   </div>   
+ </div>`;
+portfolio.innerHTML = projects;
+
+const modalContainer = document.getElementById('modal');
+
+// eslint-disable-next-line no-unused-vars
+function showModal() {
+  modalContainer.style.display = 'flex';
+}
+// eslint-disable-next-line no-unused-vars
+function closeModal() {
+  modalContainer.style.display = 'none';
+}
