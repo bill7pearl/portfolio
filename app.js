1630dbc5a3831bc717da2b26db9d400d67beae19
Billal Chami <billal7chami@gmail.com>
2023-07-02 22:43:09 +0100

    Update javascript code

--- app.js
+++ app.js
@@ -28,6 +28,10 @@ menuItems.forEach(
   },
 );
 
+function isOdd(number) {
+  return number % 2 !== 0;
+}
+
 // JS projects data //
 const projectsData = [
   {
@@ -41,7 +45,8 @@ const projectsData = [
     seeSource: '#',
   },
   {
-    title: 'Multi-post<br>Stories',
+    id: 2,
+    title: 'Multi-post Stories',
     infos: ['CANOPY', 'Back End Dev', 2015],
     description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
     languages: ['HTML', 'CSS', 'JS'],
@@ -51,7 +56,7 @@ const projectsData = [
   },
   {
     id: 3,
-    title: 'Multi-post<br>Stories',
+    title: 'Multi-post Stories',
     infos: ['CANOPY', 'Back End Dev', 2015],
     description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
     languages: ['HTML', 'CSS', 'JS'],
@@ -60,11 +65,22 @@ const projectsData = [
     seeSource: '#',
   },
   {
-    title: 'Multi-post<br>Stories',
+    id: 4,
+    title: 'Multi-post Stories',
     infos: ['CANOPY', 'Back End Dev', 2015],
     description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
     languages: ['HTML', 'CSS', 'JS'],
     image: 'images/div4-bg.png',
+    seeLive: '#',
+    seeSource: '#',
+  },
+  {
+    id: 5,
+    title: 'Test',
+    infos: ['BREW', 'Front-end Dev', 2017],
+    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
+    languages: ['HTML', 'CSS', 'JS'],
+    image: 'images/div5-bg.jpg',
     seeLive: '#',
     seeSource: '#',
   },
@@ -73,7 +89,48 @@ const projectsData = [
 // projects container //
 
 const portfolio = document.getElementById('portfolio');
-const projects = `
+let projects = '';
+for (let key in projectsData) {
+  if (projectsData.hasOwnProperty(key)) {
+    projects += `<div class="project1" ${projectsData[key].id}>
+    <div class="project1-desk-left">
+        <img class="hide-from-desk" src="${projectsData[key].image}" alt="portfolio one">
+    </div>
+    <div class="project1-desk-right">
+        <h2 class="tonic-title">${projectsData[key].title}</h2>
+     <div class="infos">
+        <p class="canopy">${projectsData[key].infos[0]}</p>
+        <img class="dot" src="images/dot.png" alt="separator">
+        <p class="tech-date">${projectsData[key].infos[1]}</p>
+        <img class="dot" src="images/dot.png" alt="separator">
+        <p class="tech-date">${projectsData[key].infos[2]}</p>  
+     </div>
+    <p class="projects-description">A daily selection of privately personalized reads; no accounts or sign-ups required.</p>
+     <ul class="project-languages">
+        <li class="project-list">${projectsData[key].languages[0]}</li>
+        <li class="project-list">${projectsData[key].languages[1]}</li>
+        <li class="project-list">${projectsData[key].languages[2]}</li>
+     </ul>
+     <button onclick="showModal()" class="projects-btn" id="btn-project-1">See Project</button>
+    </div> 
+  </div>`
+  }
+  document.addEventListener('DOMContentLoaded', function() {
+  const rowReverse = document.querySelector(`div[class$="${projectsData[key].id}"]`);
+  if (isOdd(projectsData[key].id)) {
+  rowReverse.style.flexDirection = 'row-reverse';
+  console.log(projectsData[key].id)
+  }
+  });
+  
+}
+portfolio.innerHTML = projects;
+
+
+/* if(projectsData.id = 1) {
+  rowReverse.style.flexDirection = 'row-reverse'
+} */
+/* const projects = `
    <div class="project1">
    <div class="project1-desk-left">
        <img class="hide-from-desk" src="${projectsData[0].image}" alt="portfolio one">
@@ -168,7 +225,7 @@ const projects = `
     <button onclick="showModal()" class="projects-btn">See Project</button>
    </div>   
  </div>`;
-portfolio.innerHTML = projects;
+portfolio.innerHTML = projects; */
 
 const modalContainer = document.getElementById('modal');
 
